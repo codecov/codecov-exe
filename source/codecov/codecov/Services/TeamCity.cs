5458951a85307943aa9f014b858bd2f20da7a477
Larz White <whitel20ster@gmail.com>
2017-02-24 22:26:44 -0600

    added suppport for travis ci and teamcity.

---
+++ source/codecov/codecov/Services/TeamCity.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using codecov.Services.Utils;
+
+namespace codecov.Services
+{
+    internal class TeamCity : Service
+    {
+        public TeamCity()
+        {
+            QueryParameters["branch"] = Branch;
+            QueryParameters["commit"] = Commit;
+            QueryParameters["build"] = Environment.GetEnvironmentVariable("TEAMCITY_BUILD_ID");
+            QueryParameters["build_url"] = BuildUrl;
+            QueryParameters["service"] = "teamcity";
+        }
+
+        public override bool Detect
+        {
+            get
+            {
+                var teamCity = Environment.GetEnvironmentVariable("TEAMCITY_VERSION");
+
+                if (string.IsNullOrWhiteSpace(teamCity))
+                {
+                    return false;
+                }
+
+                Console.WriteLine("==> TeamCity CI detected.");
+                return true;
+            }
+        }
+
+        private static string Branch
+        {
+            get
+            {
+                var branch = Environment.GetEnvironmentVariable("TEAMCITY_BUILD_BRANCH");
+                if (!string.IsNullOrWhiteSpace(branch))
+                {
+                    return branch;
+                }
+
+                Console.WriteLine("    Teamcity does not automatically make build parameters available as environment variables.");
+                Console.WriteLine("    Add the following environment parameters to the build configuration");
+                Console.WriteLine("    env.TEAMCITY_BUILD_BRANCH = %teamcity.build.branch%");
+                Console.WriteLine("    env.TEAMCITY_BUILD_ID = %teamcity.build.id%");
+                Console.WriteLine("    env.TEAMCITY_BUILD_URL = %teamcity.serverUrl%/viewLog.html?buildId=%teamcity.build.id%");
+                Console.WriteLine("    env.TEAMCITY_BUILD_COMMIT = %system.build.vcs.number%");
+                Console.WriteLine("    env.TEAMCITY_BUILD_REPOSITORY = %vcsroot.<YOUR TEAMCITY VCS NAME>.url%");
+                return string.Empty;
+            }
+        }
+
+        private static string BuildUrl
+        {
+            get
+            {
+                var buildUrl = Environment.GetEnvironmentVariable("TEAMCITY_BUILD_URL");
+                return !string.IsNullOrWhiteSpace(buildUrl) ? Uri.EscapeDataString(buildUrl) : string.Empty;
+            }
+        }
+
+        private static string Commit
+        {
+            get
+            {
+                var commit = Environment.GetEnvironmentVariable("TEAMCITY_BUILD_COMMIT");
+                return !string.IsNullOrWhiteSpace(commit) ? commit : Environment.GetEnvironmentVariable("BUILD_VCS_NUMBER");
+            }
+        }
+    }
+}
---
+++ source/codecov/codecov/Services/Travis.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using codecov.Services.Utils;
+
+namespace codecov.Services
+{
+    internal class Travis : Service
+    {
+        public Travis()
+        {
+            QueryParameters["branch"] = Environment.GetEnvironmentVariable("TRAVIS_BRANCH");
+            QueryParameters["commit"] = Environment.GetEnvironmentVariable("TRAVIS_COMMIT");
+            QueryParameters["job"] = Environment.GetEnvironmentVariable("TRAVIS_JOB_ID");
+            QueryParameters["build"] = Environment.GetEnvironmentVariable("TRAVIS_JOB_NUMBER");
+            QueryParameters["pr"] = Environment.GetEnvironmentVariable("TRAVIS_PULL_REQUEST");
+            QueryParameters["slug"] = Environment.GetEnvironmentVariable("TRAVIS_REPO_SLUG");
+            QueryParameters["tag"] = Environment.GetEnvironmentVariable("TRAVIS_TAG");
+            QueryParameters["service"] = "travis";
+        }
+
+        public override bool Detect
+        {
+            get
+            {
+                var travis = Environment.GetEnvironmentVariable("TRAVIS");
+                var ci = Environment.GetEnvironmentVariable("CI");
+
+                if (string.IsNullOrWhiteSpace(travis) || string.IsNullOrWhiteSpace(ci))
+                {
+                    return false;
+                }
+
+                var isTravis = travis.Equals("True") && ci.Equals("True");
+                if (!isTravis)
+                {
+                    return false;
+                }
+                Console.WriteLine("==> Travis CI detected.");
+                return true;
+            }
+        }
+    }
+}

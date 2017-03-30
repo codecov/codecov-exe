964cc29dc278fdad0ee80161c0da602910673391
Larz White <whitel20ster@gmail.com>
2017-03-29 19:55:12 -0500

    updated choco download link

--- nuspec/chocolatey/tools/chocolateyinstall.ps1
+++ nuspec/chocolatey/tools/chocolateyinstall.ps1
@@ -1,5 +1,5 @@
 ﻿$packageName = 'codecov'
-$url = 'https://github.com/larzw/codecov-exe-portable/releases/download/0.3.0-beta/Codecov.zip'
+$url = 'https://github.com/codecov/codecov-exe/releases/download/0.3.0-Beta/Codecov.zip'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $splitUrl = $url.Split('/');
 $zipFileName = $splitUrl[$splitUrl.Length - 1]

9cab76a4e9691ed3f0807fb912fe75d0c22155c6
AdmiringWorm <kim.nordmo@gmail.com>
2019-05-04 21:46:28 +0200

    (GH-59) Updated unit tests to be platform agnostic

--- Source/Codecov.Tests/Yaml/YamlTests.cs
+++ Source/Codecov.Tests/Yaml/YamlTests.cs
@@ -1,4 +1,6 @@
-﻿using Codecov.Coverage.SourceCode;
+﻿using System.IO;
+using System.Linq;
+using Codecov.Coverage.SourceCode;
 using FluentAssertions;
 using NSubstitute;
 using Xunit;
@@ -7,57 +9,42 @@ namespace Codecov.Tests.Yaml
 {
     public class YamlTests
     {
-        [WindowsFact]
-        public void Should_Find_Yaml_File_If_Exists_On_Windows()
+        private static string _systemDrive = Path.GetPathRoot(DriveInfo.GetDrives().First().ToString());
+
+        [Theory]
+        [InlineData("codecov.yaml")]
+        [InlineData(".codecov.yaml")]
+        [InlineData("codecov.yml")]
+        [InlineData(".codecov.yml")]
+        public void Should_Find_Yaml_File_If_Exists(string configFileName)
         {
             // Given
             var sourceCode = Substitute.For<ISourceCode>();
-            sourceCode.GetAll.Returns(new[] { @"C:\Fake\Class.cs", @"C:\Fake\Interface\IClass.cs", @"C:\Fake\.git", @"C:\Fake\codecov.yaml" });
+            sourceCode.GetAll.Returns(new[] {
+                Path.Combine(_systemDrive, "Fake", "Class.cs"),
+                Path.Combine(_systemDrive, "Fake", "Interface", "IClass.cs"),
+                Path.Combine(_systemDrive, "Fake", ".git"),
+                Path.Combine(_systemDrive, "Fake", configFileName)
+            });
             var yaml = new Codecov.Yaml.Yaml(sourceCode);
 
             // When
             var fileName = yaml.FileName;
 
             // Then
-            fileName.Should().Be("codecov.yaml");
+            fileName.Should().Be(configFileName);
         }
 
-        [UnixFact]
-        public void Should_Find_Yaml_File_If_Exists_On_Unix()
+        [Fact]
+        public void Should_Return_Empty_String_If_Yaml_File_Does_Not_Exit()
         {
             // Given
             var sourceCode = Substitute.For<ISourceCode>();
-            sourceCode.GetAll.Returns(new[] { @"/Fake/Class.cs", @"/Fake/Interface/IClass.cs", @"/Fake/.git", @"/Fake/codecov.yaml" });
-            var yaml = new Codecov.Yaml.Yaml(sourceCode);
-
-            // When
-            var fileName = yaml.FileName;
-
-            // Then
-            fileName.Should().Be("codecov.yaml");
-        }
-
-        [WindowsFact]
-        public void Should_Return_Empty_String_If_Yaml_File_Does_Not_Exit_On_Windows()
-        {
-            // Given
-            var sourceCode = Substitute.For<ISourceCode>();
-            sourceCode.GetAll.Returns(new[] { @"C:\Fake\Class.cs", @"C:\Fake\Interface\IClass.cs", @"C:\Fake\.git" });
-            var yaml = new Codecov.Yaml.Yaml(sourceCode);
-
-            // When
-            var fileName = yaml.FileName;
-
-            // Then
-            fileName.Should().BeEmpty();
-        }
-
-        [UnixFact]
-        public void Should_Return_Empty_String_If_Yaml_File_Does_Not_Exit_On_Unix()
-        {
-            // Given
-            var sourceCode = Substitute.For<ISourceCode>();
-            sourceCode.GetAll.Returns(new[] { @"/Fake/Class.cs", @"/Fake/Interface/IClass.cs", @"/Fake/.git" });
+            sourceCode.GetAll.Returns(new[] {
+                Path.Combine(_systemDrive, "Fake", "Class.cs"),
+                Path.Combine(_systemDrive, "Fake", "Interface", "IClass.cs"),
+                Path.Combine(_systemDrive, "Fake", ".git")
+            });
             var yaml = new Codecov.Yaml.Yaml(sourceCode);
 
             // When

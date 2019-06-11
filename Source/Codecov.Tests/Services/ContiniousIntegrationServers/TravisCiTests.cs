8dc39e4ba742b191e33c82b8eb9a22b6045beb92
AdmiringWorm <kim.nordmo@gmail.com>
2019-06-11 21:42:28 +0200

    (GH-65) Add implementation of Travis CI detector
    fixes #65

--- Source/Codecov.Tests/Services/ContiniousIntegrationServers/TravisCiTests.cs
+++ Source/Codecov.Tests/Services/ContiniousIntegrationServers/TravisCiTests.cs
@@ -5,17 +5,17 @@ using Xunit;
 
 namespace Codecov.Tests.Services.ContiniousIntegrationServers
 {
-    public class TravisCiTests
+    public class TravisTests
     {
         [Fact]
         public void Branch_Should_Be_Empty_String_When_Environment_Variable_Does_Not_Exits()
         {
             // Given
             Environment.SetEnvironmentVariable("TRAVIS_BRANCH", null);
-            var travisCi = new TravisCi();
+            var Travis = new Travis();
 
             // When
-            var branch = travisCi.Branch;
+            var branch = Travis.Branch;
 
             // Then
             branch.Should().BeEmpty();
@@ -26,10 +26,10 @@ namespace Codecov.Tests.Services.ContiniousIntegrationServers
         {
             // Given
             Environment.SetEnvironmentVariable("TRAVIS_BRANCH", "develop");
-            var travisCi = new TravisCi();
+            var Travis = new Travis();
 
             // When
-            var branch = travisCi.Branch;
+            var branch = Travis.Branch;
 
             // Then
             branch.Should().Be("develop");
@@ -40,10 +40,10 @@ namespace Codecov.Tests.Services.ContiniousIntegrationServers
         {
             // Given
             Environment.SetEnvironmentVariable("TRAVIS_JOB_NUMBER", null);
-            var travisCi = new TravisCi();
+            var Travis = new Travis();
 
             // When
-            var build = travisCi.Build;
+            var build = Travis.Build;
 
             // Then
             build.Should().BeEmpty();
@@ -54,10 +54,10 @@ namespace Codecov.Tests.Services.ContiniousIntegrationServers
         {
             // Given
             Environment.SetEnvironmentVariable("TRAVIS_JOB_NUMBER", "5.2");
-            var travisCi = new TravisCi();
+            var Travis = new Travis();
 
             // When
-            var build = travisCi.Build;
+            var build = Travis.Build;
 
             // Then
             build.Should().Be("5.2");
@@ -68,10 +68,10 @@ namespace Codecov.Tests.Services.ContiniousIntegrationServers
         {
             // Given
             Environment.SetEnvironmentVariable("TRAVIS_COMMIT", null);
-            var travisCi = new TravisCi();
+            var Travis = new Travis();
 
             // When
-            var commit = travisCi.Commit;
+            var commit = Travis.Commit;
 
             // Then
             commit.Should().BeEmpty();
@@ -82,25 +82,25 @@ namespace Codecov.Tests.Services.ContiniousIntegrationServers
         {
             // Given
             Environment.SetEnvironmentVariable("TRAVIS_COMMIT", "123");
-            var travisCi = new TravisCi();
+            var Travis = new Travis();
 
             // When
-            var commit = travisCi.Commit;
+            var commit = Travis.Commit;
 
             // Then
             commit.Should().Be("123");
         }
 
         [Theory, InlineData(null, null), InlineData("", ""), InlineData("True", null), InlineData("True", ""), InlineData(null, "True"), InlineData("", "True"), InlineData("true", "True"), InlineData("True", "true"), InlineData("False", "True"), InlineData("True", "False"), InlineData("False", "False"), InlineData("foo", "bar")]
-        public void Detecter_Should_Be_False_When_TravisCi_Environment_Variable_Or_Ci_Environment_Variable_Does_Not_Exit_And_Both_Are_Not_Equal_To_True(string travisCiData, string ciData)
+        public void Detecter_Should_Be_False_When_Travis_Environment_Variable_Or_Ci_Environment_Variable_Does_Not_Exit_And_Both_Are_Not_Equal_To_True(string TravisData, string ciData)
         {
             // Given
-            Environment.SetEnvironmentVariable("TRAVIS", travisCiData);
+            Environment.SetEnvironmentVariable("TRAVIS", TravisData);
             Environment.SetEnvironmentVariable("CI", ciData);
-            var travisCi = new TravisCi();
+            var Travis = new Travis();
 
             // When
-            var detecter = travisCi.Detecter;
+            var detecter = Travis.Detecter;
 
             // Then
             detecter.Should().BeFalse();
@@ -109,15 +109,15 @@ namespace Codecov.Tests.Services.ContiniousIntegrationServers
         [Theory]
         [InlineData("True", "True")]
         [InlineData("true", "true")]
-        public void Detecter_Should_Be_True_When_travisCi_Environment_Variable_And_Ci_Environment_Variable_Exist_And_Both_Are_Equal_To_True(string travisCiData, string ciData)
+        public void Detecter_Should_Be_True_When_Travis_Environment_Variable_And_Ci_Environment_Variable_Exist_And_Both_Are_Equal_To_True(string TravisData, string ciData)
         {
             // Given
-            Environment.SetEnvironmentVariable("TRAVIS", travisCiData);
+            Environment.SetEnvironmentVariable("TRAVIS", TravisData);
             Environment.SetEnvironmentVariable("CI", ciData);
-            var travisCi = new TravisCi();
+            var Travis = new Travis();
 
             // When
-            var detecter = travisCi.Detecter;
+            var detecter = Travis.Detecter;
 
             // Then
             detecter.Should().BeTrue();
@@ -129,10 +129,10 @@ namespace Codecov.Tests.Services.ContiniousIntegrationServers
             // Given
             Environment.SetEnvironmentVariable("TRAVIS_JOB_ID", jobNumber);
 
-            var travisCi = new TravisCi();
+            var Travis = new Travis();
 
             // When
-            var job = travisCi.Job;
+            var job = Travis.Job;
 
             // Then
             job.Should().BeEmpty();
@@ -143,10 +143,10 @@ namespace Codecov.Tests.Services.ContiniousIntegrationServers
         {
             // Given
             Environment.SetEnvironmentVariable("TRAVIS_JOB_ID", "15657");
-            var travisCi = new TravisCi();
+            var Travis = new Travis();
 
             // When
-            var job = travisCi.Job;
+            var job = Travis.Job;
 
             // Then
             job.Should().Be("15657");
@@ -157,10 +157,10 @@ namespace Codecov.Tests.Services.ContiniousIntegrationServers
         {
             // Given
             Environment.SetEnvironmentVariable("TRAVIS_PULL_REQUEST", null);
-            var travisCi = new TravisCi();
+            var Travis = new Travis();
 
             // When
-            var pr = travisCi.Pr;
+            var pr = Travis.Pr;
 
             // Then
             pr.Should().BeEmpty();
@@ -171,10 +171,10 @@ namespace Codecov.Tests.Services.ContiniousIntegrationServers
         {
             // Given
             Environment.SetEnvironmentVariable("TRAVIS_PULL_REQUEST", "123");
-            var travisCi = new TravisCi();
+            var Travis = new Travis();
 
             // When
-            var pr = travisCi.Pr;
+            var pr = Travis.Pr;
 
             // Then
             pr.Should().Be("123");
@@ -185,10 +185,10 @@ namespace Codecov.Tests.Services.ContiniousIntegrationServers
         {
             // Given
             Environment.SetEnvironmentVariable("TRAVIS_REPO_SLUG", null);
-            var travisCi = new TravisCi();
+            var Travis = new Travis();
 
             // When
-            var slug = travisCi.Slug;
+            var slug = Travis.Slug;
 
             // Then
             slug.Should().BeEmpty();
@@ -199,10 +199,10 @@ namespace Codecov.Tests.Services.ContiniousIntegrationServers
         {
             // Given
             Environment.SetEnvironmentVariable("TRAVIS_REPO_SLUG", "foo/bar");
-            var travisCi = new TravisCi();
+            var Travis = new Travis();
 
             // When
-            var slug = travisCi.Slug;
+            var slug = Travis.Slug;
 
             // Then
             slug.Should().Be("foo/bar");
---
+++ Source/Codecov/Services/ContinuousIntegrationServers/Travis.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Codecov.Utilities;
+
+namespace Codecov.Services.ContinuousIntegrationServers
+{
+    internal class Travis : ContinuousIntegrationServer
+    {
+        private readonly Lazy<string> _branch = new Lazy<string>(() => EnviornmentVariable.GetEnviornmentVariable("TRAVIS_BRANCH"));
+        private readonly Lazy<string> _build = new Lazy<string>(() => EnviornmentVariable.GetEnviornmentVariable("TRAVIS_JOB_NUMBER"));
+        private readonly Lazy<string> _commit = new Lazy<string>(() => EnviornmentVariable.GetEnviornmentVariable("TRAVIS_COMMIT"));
+        private readonly Lazy<bool> _detecter = new Lazy<bool>(LoadDetecter);
+        private readonly Lazy<string> _job = new Lazy<string>(() => EnviornmentVariable.GetEnviornmentVariable("TRAVIS_JOB_ID"));
+        private readonly Lazy<string> _pr = new Lazy<string>(() => EnviornmentVariable.GetEnviornmentVariable("TRAVIS_PULL_REQUEST"));
+        private readonly Lazy<string> _slug = new Lazy<string>(() => EnviornmentVariable.GetEnviornmentVariable("TRAVIS_REPO_SLUG"));
+
+        public override string Branch => _branch.Value;
+
+        public override string Build => _build.Value;
+
+        public override string Commit => _commit.Value;
+
+        public override bool Detecter => _detecter.Value;
+
+        public override string Job => _job.Value;
+
+        public override string Pr => _pr.Value;
+
+        public override string Service => "travis";
+
+        public override string Slug => _slug.Value;
+
+        private static bool LoadDetecter()
+        {
+            var appVeyor = EnviornmentVariable.GetEnviornmentVariable("TRAVIS");
+            var ci = EnviornmentVariable.GetEnviornmentVariable("CI");
+
+            if (string.IsNullOrWhiteSpace(appVeyor) || string.IsNullOrWhiteSpace(ci))
+            {
+                return false;
+            }
+
+            return appVeyor.Equals(ci, StringComparison.Ordinal)
+                   && appVeyor.Equals("True", StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}

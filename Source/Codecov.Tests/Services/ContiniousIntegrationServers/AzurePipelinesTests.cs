51318ea2eedbeb62e92ee85f52ffceb09485fb6b
Sam Harwell <sam@tunnelvisionlabs.com>
2019-11-12 08:29:30 -0800

    Set all properties for Azure Pipelines uploads
    Make sure to set the build_url, project, and server_uri properties for
    Azure Pipelines. These properties allow the uploads to omit the codecov
    token.

--- Source/Codecov.Tests/Services/ContiniousIntegrationServers/AzurePipelinesTests.cs
+++ Source/Codecov.Tests/Services/ContiniousIntegrationServers/AzurePipelinesTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Codecov.Services.ContinuousIntegrationServers;
 using FluentAssertions;
 using Xunit;
@@ -7,6 +8,29 @@ namespace Codecov.Tests.Services.ContiniousIntegrationServers
 {
     public class AzurePipelinesTests
     {
+        public static IEnumerable<object[]> Build_Url_Empty_Data
+        {
+            get
+            {
+                var possibleDomains = new[]{ null, string.Empty, "https://dev.azure.com/", "http://localhost:5234/" };
+                var possibleDummies = new[] { null, string.Empty, "foo", "bar" };
+
+                foreach (var domain in possibleDomains)
+                {
+                    foreach (var project in possibleDummies)
+                    {
+                        foreach (var build in possibleDummies)
+                        {
+                            if (string.IsNullOrEmpty(domain) || string.IsNullOrEmpty(project) || string.IsNullOrEmpty(build))
+                            {
+                                yield return new object[] { domain, project, build };
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
         [Fact]
         public void Branch_Should_Be_Empty_String_When_Enviornment_Variable_Does_Not_Exits()
         {
@@ -105,6 +129,57 @@ namespace Codecov.Tests.Services.ContiniousIntegrationServers
             detecter.Should().BeFalse();
         }
 
+        [Theory, MemberData(nameof(Build_Url_Empty_Data))]
+        public void BuildUrl_Should_Be_Empty_String_When_Environment_Variables_Do_Not_Exist(string serverUrl, string project, string build)
+        {
+            // Given
+            Environment.SetEnvironmentVariable("SYSTEM_TEAMFOUNDATIONSERVERURI", serverUrl);
+            Environment.SetEnvironmentVariable("SYSTEM_TEAMPROJECT", project);
+            Environment.SetEnvironmentVariable("BUILD_BUILDID", build);
+
+            var pipelines = new AzurePipelines();
+
+            // When
+            var buildUrl = pipelines.BuildUrl;
+
+            // Then
+            buildUrl.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void BuildUrl_Should_Not_Empty_String_When_Environment_Variable_Exists()
+        {
+            // Given
+            Environment.SetEnvironmentVariable("SYSTEM_TEAMFOUNDATIONSERVERURI", "https://dev.azure.com/");
+            Environment.SetEnvironmentVariable("SYSTEM_TEAMPROJECT", "project");
+            Environment.SetEnvironmentVariable("BUILD_BUILDID", "build");
+
+            var pipelines = new AzurePipelines();
+
+            // When
+            var buildUrl = pipelines.BuildUrl;
+
+            // Then
+            buildUrl.Should().Be("https://dev.azure.com/project/_build/results?buildId=build");
+        }
+
+        [Theory, InlineData("http://"), InlineData("http://."), InlineData("http://.."), InlineData("http://../"), InlineData("http://?"), InlineData("http://??"), InlineData("http://#"), InlineData("http://##"), InlineData("//"), InlineData("//a"), InlineData("///a"), InlineData("///"), InlineData("foo.com"), InlineData("rdar://1234"), InlineData("h://test"), InlineData("http:// shouldfail.com"), InlineData(":// should fail"), InlineData("ftps://foo.bar/"), InlineData("http://.www.foo.bar/")]
+        public void BuildUrl_Should_Be_Empty_When_Appveyor_Url_Is_Invalid_Domain(string urlData)
+        {
+            // Given
+            Environment.SetEnvironmentVariable("SYSTEM_TEAMFOUNDATIONSERVERURI", urlData);
+            Environment.SetEnvironmentVariable("SYSTEM_TEAMPROJECT", "project");
+            Environment.SetEnvironmentVariable("BUILD_BUILDID", "build");
+
+            var pipelines = new AzurePipelines();
+
+            // When
+            var buildUrl = pipelines.BuildUrl;
+
+            // Then
+            buildUrl.Should().BeEmpty();
+        }
+
         [Theory, InlineData(null, null), InlineData("", ""), InlineData("foo", ""), InlineData("", "foo")]
         public void Job_Should_Be_Empty_String_When_Enviornment_Variables_Do_Not_Exit(string slugData, string versionData)
         {
@@ -162,6 +237,62 @@ namespace Codecov.Tests.Services.ContiniousIntegrationServers
 
             // Then
             pr.Should().Be("123");
+        }
+
+        [Fact]
+        public void Project_Should_Be_Empty_String_When_Enviornment_Variable_Does_Not_Exits()
+        {
+            // Given
+            Environment.SetEnvironmentVariable("SYSTEM_TEAMPROJECT", null);
+            var pipelines = new AzurePipelines();
+
+            // When
+            var project = pipelines.Project;
+
+            // Then
+            project.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void Project_Should_Be_Set_When_Enviornment_Variable_Exits()
+        {
+            // Given
+            Environment.SetEnvironmentVariable("SYSTEM_TEAMPROJECT", "123");
+            var pipelines = new AzurePipelines();
+
+            // When
+            var project = pipelines.Project;
+
+            // Then
+            project.Should().Be("123");
+        }
+
+        [Fact]
+        public void ServerUri_Should_Be_Empty_String_When_Enviornment_Variable_Does_Not_Exits()
+        {
+            // Given
+            Environment.SetEnvironmentVariable("SYSTEM_TEAMFOUNDATIONSERVERURI", null);
+            var pipelines = new AzurePipelines();
+
+            // When
+            var serverUri = pipelines.ServerUri;
+
+            // Then
+            serverUri.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void ServerUri_Should_Be_Set_When_Enviornment_Variable_Exits()
+        {
+            // Given
+            Environment.SetEnvironmentVariable("SYSTEM_TEAMFOUNDATIONSERVERURI", "123");
+            var pipelines = new AzurePipelines();
+
+            // When
+            var serverUri = pipelines.ServerUri;
+
+            // Then
+            serverUri.Should().Be("123");
         }
 
         [Fact]
--- Source/Codecov/Services/ContinuousIntegrationServers/AzurePipelines.cs
+++ Source/Codecov/Services/ContinuousIntegrationServers/AzurePipelines.cs
@@ -7,15 +7,25 @@ namespace Codecov.Services.ContinuousIntegrationServers
     {
         private readonly Lazy<string> _branch = new Lazy<string>(() => EnviornmentVariable.GetEnviornmentVariable("BUILD_SOURCEBRANCHNAME"));
         private readonly Lazy<string> _build = new Lazy<string>(LoadBuild);
+        private readonly Lazy<string> _buildUrl;
         private readonly Lazy<string> _commit = new Lazy<string>(() => EnviornmentVariable.GetEnviornmentVariable("BUILD_SOURCEVERSION"));
         private readonly Lazy<bool> _detecter = new Lazy<bool>(() => CheckEnvironmentVariables("TF_BUILD"));
         private readonly Lazy<string> _job = new Lazy<string>(LoadJob);
         private readonly Lazy<string> _pr = new Lazy<string>(() => EnviornmentVariable.GetEnviornmentVariable("SYSTEM_PULLREQUEST_PULLREQUESTNUMBER"));
+        private readonly Lazy<string> _project = new Lazy<string>(() => EnviornmentVariable.GetEnviornmentVariable("SYSTEM_TEAMPROJECT"));
+        private readonly Lazy<string> _serverUri = new Lazy<string>(() => EnviornmentVariable.GetEnviornmentVariable("SYSTEM_TEAMFOUNDATIONSERVERURI"));
         private readonly Lazy<string> _slug = new Lazy<string>(() => EnviornmentVariable.GetEnviornmentVariable("BUILD_REPOSITORY_NAME"));
+
+        public AzurePipelines()
+        {
+            _buildUrl = new Lazy<string>(LoadBuildUrl);
+        }
 
         public override string Branch => _branch.Value;
 
         public override string Build => _build.Value;
+
+        public override string BuildUrl => _buildUrl.Value;
 
         public override string Commit => _commit.Value;
 
@@ -24,6 +34,10 @@ namespace Codecov.Services.ContinuousIntegrationServers
         public override string Job => _job.Value;
 
         public override string Pr => _pr.Value;
+
+        public override string Project => _project.Value;
+
+        public override string ServerUri => _serverUri.Value;
 
         public override string Service => "azure_pipelines";
 
@@ -48,6 +62,25 @@ namespace Codecov.Services.ContinuousIntegrationServers
             var job = $"{slug}/{version}";
 
             return job;
+        }
+
+        private string LoadBuildUrl()
+        {
+            var serverUri = ServerUri;
+            var project = Project;
+            var build = Build;
+            if (string.IsNullOrEmpty(serverUri) || string.IsNullOrEmpty(project) || string.IsNullOrEmpty(build))
+            {
+                return string.Empty;
+            }
+
+            if (!Uri.TryCreate(serverUri, UriKind.Absolute, out var uri)
+                || (uri.Scheme != Uri.UriSchemeHttp && uri.Scheme != Uri.UriSchemeHttps))
+            {
+                return string.Empty;
+            }
+
+            return $"{serverUri}{project}/_build/results?buildId={build}";
         }
     }
 }
